c01a011db6e8e23f1378cc839fadd3e775ea54e5
Akarsh28-collab <akarshpratapsingh2806@gmail.com>
2026-08-01 14:45:51 +0530

    Update 5insertposition.cpp

--- 5insertposition.cpp
+++ 5insertposition.cpp
@@ -1,35 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int arr[1000];
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int st = 0, end = nums.size() - 1;
+        int ans = nums.size();              // default: sabse aakhir mein insert
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (nums[mid] >= target) {      // == aur > dono merge
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
     }
-    int key;
-    cin>>key;
-
-    int st=0;
-    int end=n-1;
-    int ans=n;
-    int mid;
-    while(st<=end){
-        mid=end+(st-end)/2;
-
-        if(arr[mid]==key){
-            ans=mid;
-            break;
-        }
-        else if(arr[mid]>key){
-            ans=mid;
-            end=mid-1;
-            
-
-        }
-        else{
-            st=mid+1;
-        }
-    }
-    cout<<ans<<endl;
-}
+};
